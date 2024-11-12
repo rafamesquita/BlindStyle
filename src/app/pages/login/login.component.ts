573f1126e1be0c0a42fe22615cf2b51f20fcad7a
Gabriela Valente <gabrielalimborco@gmail.com>
2024-11-12 13:06:37 -0300

    autenticação, token, refresh_token

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,18 +2,39 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { Router } from '@angular/router';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { AuthService } from '../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms'; 
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {}
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
-  home(){
-    this.router.navigate(['/home']);
+  constructor(private authService: AuthService, private router: Router) {}
+
+  home(): void {
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        console.log('Usuário logado com sucesso');
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Erro no login', error);
+        this.errorMessage = error?.error?.detail || 'Erro desconhecido';
+      }
+    );
   }
+
+    logout(): void {
+      this.authService.logout();
+      this.router.navigate(['/login']);
+    }
 }
