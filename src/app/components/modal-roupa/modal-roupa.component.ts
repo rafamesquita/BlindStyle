a6964ec7d0dd795fb08c532477da8dafbe51d1bf
Rafaela Mesquita <rafaelamesquita02@hotmail.com>
2024-11-18 20:50:33 -0300

    modal

--- src/app/components/modal-roupa/modal-roupa.component.ts
+++ src/app/components/modal-roupa/modal-roupa.component.ts
@@ -16,7 +16,8 @@ export class ModalRoupaComponent implements OnInit{
   clothe: any
 
   ngOnInit(): void {
-      this.getSpecificClothe();
+    this.getSpecificClothe();
+    this.convertBase64ToJpg(this.data.image_url);
   }
 
   getSpecificClothe() {
@@ -30,4 +31,17 @@ export class ModalRoupaComponent implements OnInit{
       }
     })
   }
+
+  convertBase64ToJpg(base64Data: string): void {
+    // Remove o prefixo "data:image/png;base64," ou similar
+    const base64Content = base64Data.replace(/^data:image\/\w+;base64,/, '');
+  
+    // Converte o conteúdo Base64 para um Blob
+    const byteCharacters = atob(base64Content);
+    const byteNumbers = new Array(byteCharacters.length).fill(null).map((_, i) => byteCharacters.charCodeAt(i));
+    const byteArray = new Uint8Array(byteNumbers);
+  
+    const blob = new Blob([byteArray], { type: 'image/jpeg' });
+  }
+  
 }
