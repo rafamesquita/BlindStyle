55996cf8457540f322f4ddda66665f03ac561682
Gabriela Valente <gabrielalimborco@gmail.com>
2024-11-24 15:09:26 -0300

    ajuste apontamento build variaveis de ambiente

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environments';
 import { AuthService } from './auth/auth.service';
+import { environment } from '../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   private apiUrl = environment.apiUrl;
-
+  
   constructor(
     private http: HttpClient,
     private authService: AuthService
